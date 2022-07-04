270f7e1d886a9a5eb45384ffbd92fa1c077b8a22
yoojj1990 <yoojj1990@gmau>
2022-07-04 12:44:29 +0900

    리엑트 스터디

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1><a href="/">REACT</a></h1>
+      <h1>IS GOOD!!</h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li><a href="/">html</a></li>
+        <li><a href="/">css</a></li>
+        <li><a href="/">javascript</a></li>
+      </ol>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, React
+      <img src=''></img>
+    </article>
+  );
+}
+
+function Hello(props) {
+  return (
+    <div>
+      안녕하세요 저는 {props.name}입니다. 취미는 {props.habby}입니다.
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+      <Hello name="리엑트!" habby="운동"></Hello>
+      <Hello name="스프링!" habby="노래"></Hello>
     </div>
   );
 }
