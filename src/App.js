40b137405863c758c14712b903fa345626a97001
yoojj1990 <yoojj1990@gmau>
2022-07-04 15:33:38 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import './App.css';
 
 function Header(props) {
@@ -19,7 +20,7 @@ function Nav(props) {
     let dto = props.topics[i];
     list.push(<li key={dto.id}><a id={dto.id} href={"/"+dto.id} onClick={(event)=>{
       event.preventDefault();
-      props.onClickMode(event.target.id);
+      props.onClickMode(Number(event.target.id));
     }}>{dto.title}</a></li>)
   }
 
@@ -44,17 +45,38 @@ function Article(props) {
 }
 
 function App() {
+
+  const [mode, setMode] = useState("Welcome");
+  const [id, setId] = useState(null);
+
   const topics = [
     {id:1, title:"html", body:"html is Good"}, 
     {id:2, title:"css", body:"css is Good"}, 
     {id:3, title:"javascript", body:"javascript is Good"}
   ]
 
+  let content = null;
+
+  if(mode === "Welcome") {
+    content = <Article title="Welcome" body="Hello, React"></Article>
+  } else if(mode === "Read") {
+    let title, body = null;
+    for(let i=0;i<topics.length;i++){
+      if(topics[i].id === id) {
+        title = topics[i].title
+        body = topics[i].body
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  } else if(mode === "Welcome1") {
+      content = <Article title="React!" body="React is Good"></Article>
+  }
+
   return (
     <div>
-      <Header title="React!!" onClickMode={()=>{alert('React is Good!')}}></Header>
-      <Nav topics={topics} onClickMode={(id)=>{alert(id);}}></Nav>
-      <Article title="Welcome" body="Hello, React"></Article>
+      <Header title="React!!" onClickMode={()=>{setMode("Welcome1");}}></Header>
+      <Nav topics={topics} onClickMode={(id)=>{setMode("Read"); setId(id);}}></Nav>
+      {content}
     </div>
   );
 }
