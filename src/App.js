663fb45dd80e61e8aeb1a0e2b6ac9d2ac38864bb
yoojj1990 <yoojj1990@gmau>
2022-07-05 15:01:38 +0900

    리스트 추가기능 추가

--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ function Nav(props) {
 
   const list = []
   for(let i=0;i<props.topics.length;i++) {
-    let dto = props.topics[i];
-    list.push(<li key={dto.id}><a id={dto.id} href={"/"+dto.id} onClick={(event)=>{
+    let topics = props.topics[i];
+    list.push(<li key={topics.id}><a id={topics.id} href={"/"+topics.id} onClick={(event)=>{
       event.preventDefault();
       props.onClickMode(Number(event.target.id));
-    }}>{dto.title}</a></li>)
+    }}>{topics.title}</a></li>)
   }
 
   return (
@@ -40,7 +40,24 @@ function Article(props) {
     <article>
       <h2>{props.title}</h2>
       {props.body}
-      <img src=''></img>
+    </article>
+  );
+}
+
+function Create(props) {
+  return (
+    <article>
+      <h2>새로운 리스트 생성</h2>
+      <form onSubmit={(event)=>{
+        event.preventDefault();
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <input type="text" name="title"></input><br></br>
+        <textarea name="body"></textarea><br></br>
+        <input type="submit" value="등록"></input>
+      </form>
     </article>
   );
 }
@@ -49,12 +66,14 @@ function App() {
 
   const [mode, setMode] = useState("Welcome");
   const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
 
-  const topics = [
+
+  const [topics, setTopics] = useState([
     {id:1, title:"html", body:"html is Good"}, 
     {id:2, title:"css", body:"css is Good"}, 
     {id:3, title:"javascript", body:"javascript is Good"}
-  ]
+  ]);
 
   let content = null;
 
@@ -71,6 +90,16 @@ function App() {
     content = <Article title={title} body={body}></Article>
   } else if(mode === "Welcome1") {
       content = <Article title="React!" body="React is Good"></Article>
+  } else if(mode === "Create") {
+      content = <Create onCreate={(title, body) => {
+        const newTopic = {id:nextId, title:title, body:body}
+        setId(nextId);
+        setNextId(nextId+1);
+        const newTopics = [...topics]
+        newTopics.push(newTopic);
+        setTopics(newTopics);
+        setMode("Read");
+      }}></Create>
   }
 
   return (
@@ -78,6 +107,11 @@ function App() {
       <Header title="React!!" onClickMode={()=>{setMode("Welcome1");}}></Header>
       <Nav topics={topics} onClickMode={(id)=>{setMode("Read"); setId(id);}}></Nav>
       {content}
+      <a href="/create" onClick={(event)=>
+        {
+          event.preventDefault();
+          setMode("Create");
+        }}>New List Create</a>
     </div>
   );
 }
